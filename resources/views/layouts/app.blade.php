715be6155d02026590e4a108a7946fff8f17117c
unknown <yopannalpiss@gmail.com>
2024-06-25 23:52:25 +0700

    some FIXING

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,11 @@
     <!-- Vendor CSS Files -->
     <link href="{{ URL::asset('build/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
     <link href="{{ URL::asset('build/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('build/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('build/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('build/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('build/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('build/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet" />
+    {{-- <link href="{{ URL::asset('build/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet" /> --}}
+    {{-- <link href="{{ URL::asset('build/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet" />
+    <link href="{{ URL::asset('build/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet" /> --}}
+    {{-- <link href="{{ URL::asset('build/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet" /> --}}
+    {{-- <link href="{{ URL::asset('build/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet" /> --}}
 
     <!-- Vendor JS Files -->
     <script src="{{ URL::asset('build/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
